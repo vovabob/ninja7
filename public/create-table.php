542ea4185f3247064fade2be4942fd1061b36a63
bob@i5v <bob@i5v>
2022-03-20 02:43:06 +1300

    public/create-table.php MOD.

--- public/create-table.php
+++ public/create-table.php
@@ -1,15 +1,23 @@
 <?php
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=db1;charset=utf8mb4', 'user1', 'qqq');
-    $output = 'Database connection established.';
+    $output = 'Database connection established.' . '<br>';
     $sql = 'CREATE TABLE IF NOT EXISTS joke (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	joketext TEXT,
 	jokedate DATE NOT NULL
 	) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
 
-    $pdo->exec($sql);
-    $output = 'Joke table successfully created.';
+    $retVal = $pdo->exec($sql);
+
+    switch ($retVal) {
+    	case 0:
+       	    $output .= 'No rows affected.';
+	    break;
+	case ($retVal > 0):
+	    $output .= $output . ' rows have been modified.';
+	    break;
+    }
 
 } catch (PDOException $e) {
     $output = 'Error: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
